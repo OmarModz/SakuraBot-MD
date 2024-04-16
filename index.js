54c0b6d4c883f3b6e1ddfa1e7a03325fbfe8f85d
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+diegojadibot@users.noreply.github.com>
2024-04-16 09:44:32 -0500

    Actualizar index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const rl = createInterface(process.stdin, process.stdout)
 const app = express()
 const port = process.env.PORT || 8080;
 
-say('Nino Nakano - MD', {
+say('Sakura\nBot-MD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
